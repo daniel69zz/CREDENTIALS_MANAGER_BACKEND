6e435f6f4c8bd5756f1980046fa72ee77e960c75
daniel69zz <luisdanielrojascaceres@gmail.com>
2026-03-03 11:05:21 -0400

    3_3_v4

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const app = require("./src/app");
 
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "0.0.0.0";
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Servidor corriendo en http://${HOST}:${PORT}`);
 });
